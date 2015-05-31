7e2a5fe0e71b7203095a6fbb9c301a4a1d1569d6
Hyeonseok Shin <hyeonseok@gmail.com>
2015-05-31 16:34:44 +0900

    ACE-5 데이터 디렉토리 접근 막음

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -avz -e ssh --delete --exclude=logs/ ./www/ accessibility@accessibility.kr:public_html/nia/
+rsync -avz -e ssh --delete --exclude=logs/*.tsv ./www/ accessibility@accessibility.kr:public_html/nia/
